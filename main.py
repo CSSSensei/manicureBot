20ab9e7100165b1acbd35a9628ee48ce95e5c368
CSSSensei <mihail.nikolenk0@yandex.ru>
2025-07-28 01:53:45 +0300

    Delete inline_handler.py (currently no inline mode)

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from config import bot
 from bot.middlewares.get_user import GetUserMiddleware
 from bot.middlewares.shadow_ban import ShadowBanMiddleware
 from bot.middlewares.logging_query import UserRegistrationMiddleware
-from bot.handlers import callbacks, user_handlers, inline_handler, commands, admin_handlers
+from bot.handlers import callbacks, user_handlers, commands, admin_handlers
 from DB import init_database
 import logging
 
@@ -42,7 +42,6 @@ async def main() -> None:
     dp.include_router(commands.router)
     dp.include_router(user_handlers.router)
     dp.include_router(callbacks.router)
-    dp.include_router(inline_handler.router)
 
     logger.info('Including middlewares')
     dp.update.middleware(GetUserMiddleware())
