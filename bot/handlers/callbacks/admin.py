c014d9905739b3392c464a15b9fc38384536a136
CSSSensei <mihail.nikolenk0@yandex.ru>
2025-07-30 01:59:22 +0300

    Revert admin commands to version from commit 65e8ac1

--- bot/handlers/callbacks/admin.py
+++ bot/handlers/callbacks/admin.py
@@ -1,20 +1,20 @@
 from aiogram import Router
 from aiogram.types import CallbackQuery
 
-from bot.utils.models import PageCallBack
+from bot.utils.models import AdminPageCallBack
 from bot import pages
 
 router = Router()
 
 
-@router.callback_query(PageCallBack.filter())
-async def cut_message_distributor(callback: CallbackQuery, callback_data: PageCallBack):
-    action = callback_data.action
+@router.callback_query(AdminPageCallBack.filter())
+async def cut_message_distributor(callback: CallbackQuery, callback_data: AdminPageCallBack):
+    type_of_event = callback_data.type_of_event
     page = callback_data.page
     user_id = callback_data.user_id
-    if action == 1:
+    if type_of_event == 1:
         await pages.get_users(callback.from_user.id, page, callback.message.message_id)
-    elif action == 2:
+    elif type_of_event == 2:
         await pages.user_query(callback.from_user.id, user_id, page, callback.message.message_id)
-    elif action == -1:
+    elif type_of_event == -1:
         await callback.answer()
